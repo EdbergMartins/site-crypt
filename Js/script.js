37b630fdd577a8cf744c254235a7cbf6e0ee2bc6
EdberMartins <edbergmartins@gmail.com>
2022-01-24 19:33:37 -0300

    Upload do projeto completo

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+function showAndHiddenContent(element,arrow) {
+  var local = document.getElementById(element);
+  var imgArrow = document.getElementById(arrow);
+  if (local.style.display == 'none') {
+    local.style.display = 'flex';
+    imgArrow.src = "img/box5/arrowUp.png";
+
+  } else {
+    local.style.display = 'none';
+    imgArrow.src = "img/box5/ARROW _ DOWN.png";
+  }
+}
